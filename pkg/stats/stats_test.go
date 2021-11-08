33b7451a29702b370e254c6eab06ecc4de15a0b1
Leo <charmed_devil@list.ru>
2021-11-08 16:38:19 +0500

    добавлена функция CategoriesAvg и авто-тест к ней

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -101,3 +101,25 @@ func TestCategoriesTotal(t *testing.T) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}	
 }
+
+func TestCategoriesAvg(t *testing.T) {
+	payments := []types.Payment{
+		{ID: 1, Category: "auto", Amount: 100000},
+		{ID: 2, Category: "food", Amount: 200000},
+		{ID: 2, Category: "food", Amount: 100000},
+		{ID: 3, Category: "auto", Amount: 300000},
+		{ID: 4, Category: "auto", Amount: 200000},
+		{ID: 5, Category: "fun", Amount: 500000},
+	}
+	expected := map[types.Category]types.Money{
+		"auto": 200000,
+		"food": 150000,
+		"fun": 500000,
+	}
+
+	result := CategoriesAvg(payments)
+
+	if !reflect.DeepEqual(expected, result){
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}	
+}
